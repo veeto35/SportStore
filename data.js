bc61d221334ebf61162c1961741d1e83f539d9fa
veeto <killersauros01@gmail.com>
2023-11-28 23:35:21 +0000

    add: adding 2 files for data and auth

---
+++ data.js
@@ -0,0 +1,73 @@
+module.exports = function() {
+
+    return {
+        products: [
+            {
+                id: 1,
+                name: 'Kayak',
+                price: 275,
+                description: 'A boaat for one person ',
+                category: 'WaterSports'
+            },
+            {
+                id: 2,
+                name: 'Lifejacket',
+                price: 48.95,
+                description: 'Protective and fashinable',
+                category: 'WaterSports'
+            },
+            {
+                id: 3,
+                name: 'Soccer Ball',
+                price: 19.50,
+                description: 'FIFA approved weigth-size',
+                category: 'Soccer'
+            },
+            {
+                id: 4,
+                name: 'Thinking hat',
+                price: 16,
+                description: 'Improves your ability to think',
+                category: 'Chess'
+            },
+            {
+                id: 5,
+                name: 'Corner Flags',
+                price: 34.95,
+                description: 'Give your playing field a professional touch',
+                category: 'Soccer'
+            },
+            {
+                id: 6,
+                name: 'Stadium',
+                price: 795000,
+                description: 'Flat-packed 35000 seat stadium',
+                category: 'Soccer'
+            },
+            {
+                id: 7,
+                name: 'Unsteady Chair',
+                price: 29.95,
+                description: 'Secretly give your oponnent a disadvantage',
+                category: 'Chess'
+            },
+            {
+                id: 8,
+                name: 'Human Chess Board',
+                price: 75,
+                description: 'A fun game for the family',
+                category: 'Chess'
+            },
+            {
+                id: 9,
+                name: 'Bling king',
+                price: 1200,
+                description: 'Gold-plated, diamond-studded king',
+                category: 'C hess'
+            }
+        ],
+        orders: []
+    }
+
+
+}
